42d677d7e534eadf48b49ab5fde93149a19def40
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-01-22 21:00:17 +0100

    MIDDLEWARE UPLOAD PARA SUBIR FOTOS EN EL REGISTRO

--- server/routes/usuarioRoutes.js
+++ server/routes/usuarioRoutes.js
@@ -6,9 +6,16 @@ import express from "express";
 // IMPORTAMOS EL CONTROLADOR
 import { signup, login } from "../controllers/usuarioController.js";
 
+// IMPORTAMOS MIDDLEWARES
+import { upload } from "../middlewares/upload.js"; // MIDDLEWARE SUBIDA IMAGENES
+
 // ROUTER
 export const userRouter = express.Router();
 
 // ENDPOINTS
-userRouter.post("/signup", signup); // ENDPOINT REGISTRO
+userRouter.post(
+  "/signup",
+  upload.fields([{ name: "image", macCount: 1 }]),
+  signup
+); // ENDPOINT REGISTRO
 userRouter.post("/login", login); // ENDPOINT LOGIN
