effe3565cecb9e5c07c58eda79414d1d8fa07cdc
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-01-23 17:12:46 +0100

    Crear Club

--- server/controllers/clubController.js
+++ server/controllers/clubController.js
@@ -3,24 +3,59 @@
 // IMPORTAMOS EL MODELO
 import { Club } from "../models/clubModel.js";
 
+// FUNCION QUE CREA UN NUEVO CLUB
+export const createClub = async (req, res) => {
+  let club = new Club();
+  const { nombre } = req.body;
+
+  try {
+    club = await Club.findOne({
+      nombre,
+    });
+
+    if (!nombre)
+      return res.status(400).send({
+        ok: false,
+        mensaje: "Ya existe un club con ese nombre",
+      });
+
+    club = new Club(req.body);
+    await club.save();
+
+    return res.status(200).send({
+      ok: true,
+      mensaje: `Club con nombre ${nombre} añadido con éxito`,
+      club,
+    });
+  } catch (error) {
+    return res.status(500).send({
+      mensaje: "Error en el servidor",
+      error: error.message,
+    });
+  }
+};
+
 // FUNCION QUE DEVUELVE TODOS LOS CLUBES
 export const getClubes = async (req, res) => {
   try {
     const club = await Club.find();
     if (!club) {
       res.status(400).send({
+        ok: false,
         mensaje: "Error al recuperar los clubes",
       });
     }
 
     if (club) {
       res.status(200).send({
+        ok: true,
         mensaje: "Obteniendo clubes...",
         club,
       });
     }
   } catch (error) {
     res.status(500).send({
+      ok: false,
       mensaje: "Error en el servidor",
       error: error.message,
     });
--- server/routes/clubRoutes.js
+++ server/routes/clubRoutes.js
@@ -4,10 +4,11 @@
 import express from "express";
 
 // IMPORTAMOS EL CONTROLADOR
-import { getClubes } from "../controllers/clubController.js";
+import { getClubes, createClub } from "../controllers/clubController.js";
 
 // ROUTER
 export const clubRouter = express.Router();
 
 // ENDPOINTS
 clubRouter.get("/clubes", getClubes); // ENDPOINT GETCLUBES
+clubRouter.post("/clubes", createClub); // ENDPOINT CREAR CLUB
