c8ee43aacc143f40159e7f602749a744d7095b01
Alfonso Ramírez <a24rameal@iesgrancapitan.org>
2025-01-24 18:52:38 +0100

    Implementacion auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
+import "./styles/App.css";
+
+import { LoginRegister } from "./pages/LoginRegister";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<LoginRegister />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
